19313a2f139b6482af502ba5dcf2f30b4f5816fc
ydarff <lamaface200@gmail.com>
2019-09-20 01:35:10 +0700

    first commit

---
+++ app/src/main/java/com/example/sisfolab/helloworld_tekber/helloworldtekber.java
@@ -0,0 +1,13 @@
+package com.example.sisfolab.helloworld_tekber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class helloworldtekber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helloworldtekber);
+    }
+}
